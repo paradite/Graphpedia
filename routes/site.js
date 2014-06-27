cced7e192c7f993c2b51edfafb42ccb2f7a36a37
Zhu Liang <zhuliangg11@gmail.com>
2014-06-27 20:40:43 +0800

    updated routing for search, now searching a term directs to a page with the term as part of the url.

--- routes/site.js
+++ routes/site.js
@@ -7,11 +7,26 @@ exports.index = function(req, res){
 	res.render('index');
 };
 
+
 /*
-POST Search a term
+POST Direct the search to the item-specific-url
+*/
+exports.searchinit = function(req, res){
+	var name = req.body['name'];
+		if(name == null){
+		res.render('index');
+	}
+	res.redirect('/search/' + name);
+}
+
+/*
+GET Search a term
 */
 exports.search = function(req, res){
-	var name = req.body['name'];
+	var name = req.params.name;
+	if(name == null){
+		res.render('index');
+	}
     Term.getByName(name, function (err, terms) {
         if (err) return next(err);
         if(terms.length == 0){
