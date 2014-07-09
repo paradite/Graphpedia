86bfffc6821e3d560d786e7db2cdf1f419678001
Scarlett <scarlett.dongyan@gmail.com>
2014-07-10 01:02:34 +0800

    add select2

---
+++ public/select2/form.js
@@ -0,0 +1,8 @@
+$(document).ready(function() { 
+    $("#e1").select2({
+        placeholder: "Relationship",
+        allowClear: true});
+    $("#e2").select2({
+        placeholder: "Term",
+        allowClear: true});
+});
