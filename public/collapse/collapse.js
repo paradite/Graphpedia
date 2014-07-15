984106e6f204ba7a7b3b15eda96761782a663c21
Scarlett <scarlett.dongyan@gmail.com>
2014-07-16 03:46:12 +0800

    modify collapsible boxes

---
+++ public/collapse/collapse.js
@@ -0,0 +1,41 @@
+function toggle1() {
+    var ele = document.getElementById('subdiv1');
+    var text = document.getElementById('toggletext1');
+    if(ele.style.display == "block") {
+        ele.style.display = "none";
+        text.innerHTML = "show";
+    }
+    else {
+        ele.style.display = "block";
+        text.innerHTML = "hide";
+        window.location.hash = "subdiv1";
+    }
+}
+
+function toggle2() {
+    var ele = document.getElementById('subdiv2');
+    var text = document.getElementById('toggletext2');
+    if(ele.style.display == "block") {
+        ele.style.display = "none";
+        text.innerHTML = "show";
+    }
+    else {
+        ele.style.display = "block";
+        text.innerHTML = "hide";
+        window.location.hash = "subdiv2";
+    }
+}
+
+function toggle3() {
+    var ele = document.getElementById('subdiv3');
+    var text = document.getElementById('toggletext3');
+    if(ele.style.display == "block") {
+        ele.style.display = "none";
+        text.innerHTML = "show";
+    }
+    else {
+        ele.style.display = "block";
+        text.innerHTML = "hide";
+        window.location.hash = "subdiv3";
+    }
+}
