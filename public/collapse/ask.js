4113869a7a2647b800074396e6765272779c477d
Scarlett <scarlett.dongyan@gmail.com>
2014-07-24 22:12:23 +0800

    update index askBox

--- public/collapse/ask.js
+++ public/collapse/ask.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
-  $("#closeButton").click(function(){
-    $("#askBox").fadeOut(1000);
-  });
+    $("#closeButton").click(function(){
+        $("#askBox").fadeOut(1000);
+    });
+    $("#add").click(function () {
+        if ($('#e1').select2("val") != ''){
+            alert("Thanks for your contribution!");
+            window.location.replace('/');
+        }
+    });
 });
